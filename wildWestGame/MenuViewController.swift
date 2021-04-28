c3fa8a1e0bf1ec14c97b004c46952bd9c792b732
Igor Palyvoda <tib.igor.mk@gmail.com>
2021-04-28 12:53:43 +0300

    Added UI for menu Screen, added icon

---
+++ wildWestGame/MenuViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  wildWestGame
+//
+//  Created by Ingvar on 28.04.2021.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    @IBAction func playButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func placesButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func webButtonTapped(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
