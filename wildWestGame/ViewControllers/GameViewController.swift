3ba644a8a73c0c7c4dc7ede9e8d8df6a7396840e
Igor Palyvoda <tib.igor.mk@gmail.com>
2021-04-29 14:15:59 +0300

    logic for game

--- wildWestGame/ViewControllers/GameViewController.swift
+++ wildWestGame/ViewControllers/GameViewController.swift
@@ -26,17 +26,39 @@ class GameViewController: UIViewController {
     @IBAction func buttonPressed(_ sender: UIButton) {
         if sender.imageView?.image == UIImage(named: "box") {
             let image = UIImage(named: buttonImages[sender.tag - 1])
+            //let image2 = UIImage(named: "box")
             sender.setImage(image, for: .normal)
             UIView.transition(with: sender, duration: 0.3, options: .transitionFlipFromRight, animations: nil, completion: nil)
+            indexElement = 1
+            for im in buttonImages {
+                if sender.imageView?.image == UIImage(named: im)  {
+                    print("WORK")
+                    print(indexElement)
+                    if sender.tag != indexElement {
+                        scorePoints += 10
+                    }
+//                    if sender.tag == indexElement {
+//                        scorePoints += 10
+//                    } else {
+//                        sender.setImage(image2, for: .normal)
+//                    }
+                } else {
+                    print("Not equal")
+                }
+                indexElement += 1
+            }
+            print("//////////////////////////////")
         } else {
-            let image = UIImage(named: "box")
-            sender.setImage(image, for: .normal)
-            UIView.transition(with: sender, duration: 0.3, options: .transitionFlipFromLeft, animations: nil, completion: nil)
+//            let image = UIImage(named: "box")
+//            sender.setImage(image, for: .normal)
+//            UIView.transition(with: sender, duration: 0.3, options: .transitionFlipFromLeft, animations: nil, completion: nil)
         }
+        scoreLabel.text = "Score: \(scorePoints)"
     }
     
     private var buttonImages = ["cowboy","cowboy2","hat","rev","kar","dead","cowboy","cowboy2","hat","rev","kar","dead"]
     private var scorePoints = 0
+    private var indexElement = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
